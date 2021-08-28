a53375e4e7e03a1a4b98331e35948a93a02f2d97
Mercedes A <mercedes.aguilar95@hotmail.com>
2021-08-28 15:46:06 -0500

    Add files via upload
    Método Burbuja con cocatenación al imprimir los arreglos.

---
+++ Metodos de Ordenamiento/BubbleSort_concat.py
@@ -0,0 +1,16 @@
+
+def bubblesort(numbers):
+
+    for j in range(len(numbers) - 1, 0, -1):
+        for i in range(j):
+            if numbers[i] > numbers[i + 1]:
+                temp = numbers[i]
+                numbers[i] = numbers[i + 1]
+                numbers[i + 1] = temp
+
+
+arr = [7, 19, 1, 2, 13, 5, 23, 29, 11, 17, 6]
+print("El arreglo en desorden es:" + str(arr))
+bubblesort(arr)
+print("El arreglo ordenado mediante el método Bubble Sort: " + str(arr))
+#concatenación de una cadena de texto y arreglo str
